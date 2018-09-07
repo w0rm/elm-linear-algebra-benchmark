298567f97d1b9f86fe353608cbfecb803538eda1
Andrey Kuzmin <unsoundscapes@gmail.com>
2018-09-08 00:33:46 +0200

    Try benchmark lists of inputs

--- benchmarks/Mat4.elm
+++ benchmarks/Mat4.elm
@@ -31,12 +31,32 @@ suite =
         v =
             Vec3.fromRecord altV
 
+        altAllV =
+            List.range 0 100
+                |> List.map
+                    (\i ->
+                        { x = toFloat i, y = 1, z = 3 }
+                    )
+
+        altAllM =
+            List.range 0 100
+                |> List.map
+                    (\i ->
+                        AltMat4.makePerspective 30 1.5 0 (toFloat i)
+                    )
+
+        allM =
+            List.map Mat4.fromRecord altAllM
+
+        allV =
+            List.map Vec3.fromRecord altAllV
+
         altV =
             AltVec3.vec3 1 2 3
     in
     describe "Mat4"
-        [ Benchmark.compare "transform" "kernel" (\_ -> Mat4.transform a v) "elm" (\_ -> AltMat4.transform altA altV)
-        , Benchmark.compare "inverse" "kernel" (\_ -> Mat4.inverse b) "elm" (\_ -> AltMat4.inverse altB)
+        [ Benchmark.compare "transform" "kernel" (\_ -> List.map (Mat4.transform a) allV) "elm" (\_ -> List.map (AltMat4.transform altA) altAllV)
+        , Benchmark.compare "inverse" "kernel" (\_ -> List.map Mat4.inverse allM) "elm" (\_ -> List.map AltMat4.inverse altAllM)
         , Benchmark.compare "inverseOrthonormal" "kernel" (\_ -> Mat4.inverseOrthonormal b) "elm" (\_ -> AltMat4.inverseOrthonormal altB)
         , Benchmark.compare "mul" "kernel" (\_ -> Mat4.mul a b) "elm" (\_ -> AltMat4.mul altA altB)
         , Benchmark.compare "mulAffine" "kernel" (\_ -> Mat4.mulAffine a b) "elm" (\_ -> AltMat4.mulAffine altA altB)
